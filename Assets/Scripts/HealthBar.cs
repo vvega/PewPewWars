e4ad66afbea6d106fd4698c6cf5c1bda4c8079c1
Veronica Vega <vvega@MacBook-Pro.local>
2016-01-30 19:32:16 -0800

    Health bar implementation. SWEET RELEASE THANK GOD

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+
+	public float current;
+	public float max;
+
+	private float height;
+
+	// Use this for initialization
+	void Start () {
+		max = this.gameObject.GetComponent<RectTransform>().rect.width;
+		height = this.gameObject.GetComponent<RectTransform>().rect.height;
+
+		reset();
+	}
+
+	public void reset() {
+		current = max;
+		adjustWidth ();
+	}
+
+	public void updatePercentage(int currentHealth, int maxHealth) {
+		current = ((float)currentHealth/(float)maxHealth) * max;
+		adjustWidth ();
+	}
+
+	private void adjustWidth() {
+		this.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(current, height);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
