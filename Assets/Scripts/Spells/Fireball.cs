81617978cca4b3fa79afafd95892b4f87b1be1ea
JD <jaadus@gmail.com>
2016-01-31 15:34:09 -0800

    fireball blows up 10% of the other team

--- Assets/Scripts/Spells/Fireball.cs
+++ Assets/Scripts/Spells/Fireball.cs
@@ -8,6 +8,11 @@ public class Fireball : Spell {
 	public override void Cast(Team myTeam, Team enemyTeam) {
 		Debug.Log("BOOM");
 		enemyTeam.TakeDamage(damage);
+		int killCount = Mathf.Max(enemyTeam.members.Count/10, 1);
+		int startIndex = Mathf.Max(Random.Range(0, enemyTeam.members.Count-(killCount-1)), 0);
+		foreach (string enemyName in enemyTeam.members.GetRange(startIndex, killCount)) {
+			gameObject.GetComponent<GameMaster>().GetPlayerObject(enemyName).GetComponent<Player>().DealDamage(damage);
+		}
 
 		GameObject cast = Instantiate(castTemplate);
 		cast.transform.position = myTeam.gameObject.transform.position;
