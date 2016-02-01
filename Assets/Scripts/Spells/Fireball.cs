49a636123003c71bee07915dc59292e5585a04f8
JD <jaadus@gmail.com>
2016-01-31 17:01:48 -0800

    delete stuff

--- Assets/Scripts/Spells/Fireball.cs
+++ Assets/Scripts/Spells/Fireball.cs
@@ -9,7 +9,7 @@ public class Fireball : Spell {
 		Debug.Log("BOOM");
 		enemyTeam.TakeDamage(damage);
 		int killCount = Mathf.Max(enemyTeam.members.Count/10, 1);
-		int startIndex = Mathf.Max(Random.Range(0, enemyTeam.members.Count-(killCount-1)), 0);
+		int startIndex = Random.Range(0, enemyTeam.members.Count-(killCount-1));
 		foreach (string enemyName in enemyTeam.members.GetRange(startIndex, killCount)) {
 			gameObject.GetComponent<GameMaster>().GetPlayerObject(enemyName).GetComponent<Player>().DealDamage(damage);
 		}
--- Assets/Scripts/Spells/MagicMissile.cs
+++ Assets/Scripts/Spells/MagicMissile.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 
 public class MagicMissile : Spell {
-	// public float timeVisible;
 
 	[Header("Templates")]
 	public GameObject castTemplate;
@@ -9,11 +8,6 @@ public class MagicMissile : Spell {
 	public override void Cast (Team myTeam, Team enemyTeam) {
 		Debug.Log("I cast magic missile at the darkness!");
 		enemyTeam.TakeDamage(damage);
-
-		/*GameObject magicMissileObj = Instantiate(magicMissileTemplate);
-		magicMissileObj.GetComponent<LineRenderer>().SetPosition(0, myTeam.gameObject.transform.position);
-		magicMissileObj.GetComponent<LineRenderer>().SetPosition(1, enemyTeam.gameObject.transform.position);
-		Destroy(magicMissileObj, timeVisible);*/
 
 		GameObject cast = Instantiate(castTemplate);
 		cast.transform.position = myTeam.gameObject.transform.position;
