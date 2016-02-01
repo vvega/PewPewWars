07071bbfffe1ae1a4ae2ec8d16275634dfb524a3
JD <jaadus@gmail.com>
2016-01-31 18:34:35 -0800

    credits

---
+++ Assets/Scripts/Chat Commands/Credits.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Credits : ChatCommand {
+
+	public override void ProcessCommand(GameMaster gameMaster, string username, string parameters) {
+		TwitchIRC irc = gameMaster.GetComponent<TwitchIRC>();
+		irc.MessageChannel("Credits: (DrMrWaffles, Jaadus, SaucyDragon, DewPyrin, Flondrixa)");
+	}
+}
